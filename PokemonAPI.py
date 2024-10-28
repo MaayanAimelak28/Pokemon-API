7b2e4e18c527303aa31d53369ea9060b95cc22c8
maayan <maayanaimelak@gmail.com>
2024-10-28 19:11:24 +0200

    added more pokemons

--- PokemonAPI.py
+++ PokemonAPI.py
@@ -1,101 +1,71 @@
 import requests
 import json
 import random
+from dynamodb_handler import create_dynamodb_table, insert_pokemon_data, scan_pokemon_table
 
-
-# get a list of all pokemons from the PokeAPI website.
+# get a list of all Pokémon from the PokeAPI website
 def all_the_data():
     url = 'https://pokeapi.co/api/v2/pokemon/?offset=100&limit=100'
-    pokemon_json_file = requests.get(url).json() # get all the data
-    pokemon_names_and_urls = pokemon_json_file["results"] # extract names and urls
+    pokemon_json_file = requests.get(url).json()  # Get all the data
+    pokemon_names_and_urls = pokemon_json_file["results"]  # Extract names and URLs
     return pokemon_names_and_urls
 
-# return a list of pokemon names from the provided names and urls.
+# return a list of Pokémon names from the provided names and URLs
 def name_list(pokemon_names_and_urls):
-    pokemon_name_list = []
-    for pok in pokemon_names_and_urls:
-        pokemon_name_list.append(pok['name'])
-    return pokemon_name_list
+    return [pok['name'] for pok in pokemon_names_and_urls]  # List comprehension for cleaner code
 
-
-# create a JSON file containing details of a specific pokemon and return its data
-def make_json_file(pokemon_names_and_urls, defult_pokemon_index=0):
-    new_url = pokemon_names_and_urls[defult_pokemon_index]['url']
+# create a JSON file containing details of a specific Pokémon and return its data
+def make_file(pokemon_names_and_urls, default_pokemon_index=0):
+    new_url = pokemon_names_and_urls[default_pokemon_index]['url']
     data_schema = requests.get(new_url).json()
-    data = {'name': data_schema['name'], 'id': data_schema['id'], 'height': data_schema['height'],
-            'weight': data_schema['weight'], 'base_experience': data_schema['base_experience']}
-    # open the JSON file and write the pokemon data as a list
-    with open('/home/ec2-user/MaayanRepository/output.json', 'w') as f:
-        json.dump([data], f, indent=4)  # write a list with one pokemon
-
+    data = {
+        'name': data_schema['name'],
+        'id': data_schema['id'],
+        'height': data_schema['height'],
+        'weight': data_schema['weight'],
+        'base_experience': data_schema['base_experience']
+    }
     return data
 
-# extract the data from the JSON file and return it
-def extract_data_from_json():
-    # open the JSON file and read the data
-    with open('/home/ec2-user/MaayanRepository/output.json', 'r') as f:
-        loaded_data = json.load(f)
-    return loaded_data
-
-# return the index of a new pokemon in the list of names and urls
+# return the index of a new Pokémon in the list of names and URLs
 def download_details(pokemon_name, pokemon_names_and_urls):
     for index, pokemon in enumerate(pokemon_names_and_urls):
         if pokemon['name'] == pokemon_name:
             return index
 
-
-# update the JSON file with the new list of pokemons
-def update_json_file(pokemons_list):
-    with open('/home/ec2-user/MaayanRepository/output.json', 'w') as f:
-        json.dump(pokemons_list, f, indent=4)
-    return pokemons_list
-
-
-# Main function: prompt the user to draw a pokemon. If yes, randomly choose a pokemon.
-# if the chosen pokemon is in the existing list, retrieve its attributes,
-# otherwise, fetch the pokemon's details from the API and update the list.
+# Main function: prompt the user to draw a Pokémon
 def collect_pokemons():
-    data_names_urls = all_the_data()  # retrieve all the data
-    pokemon_name_list = name_list(data_names_urls) # create the pokemon name list and return it
+    data_names_urls = all_the_data()  # Retrieve all the data
+    pokemon_name_list = name_list(data_names_urls)  # Create the Pokémon name list
 
-    # initialize the JSON file and add the first pokemon to the list.
-    make_json_file(data_names_urls)
+    # initialize the JSON file and add the first Pokémon to the list and add it to DynamoDB database
+    insert_pokemon_data(make_file(data_names_urls))
 
     valid_flag = False
     print('\nWelcome to my PokemonAPI Project!\n')
-    while valid_flag == False:
+    while not valid_flag:
         user_input = input('Do you like to draw a Pokémon? (Y or N) ').lower()
 
         if user_input == 'y':
-            random_pokemon = random.choice(pokemon_name_list)  # randomly choose a pokemon
+            random_pokemon = random.choice(pokemon_name_list)  # Randomly choose a Pokémon
 
-            # check if the random_pokemon is already in output.json
-            current_pokemons = extract_data_from_json()  # get the current list of pokemons
-            # create a list of existing pokemon names from the current pokemon data
-            existing_names = []
-            for pokemon in current_pokemons:
-                existing_names.append(pokemon['name'])
-            # check if the randomly selected pokemon is already in the existing names list
-            if random_pokemon in existing_names:
-                show_data = extract_data_from_json()  # extract details for the existing pokemon
-                print('\nThis Pokemon in our names list: ' + str(random_pokemon))
-            else:  # random_pokemon is not in JSON
-                new_index_pok = download_details(random_pokemon, data_names_urls) # get index for the new pokemon
-                new_pokemon = make_json_file(data_names_urls, new_index_pok) # create JSON for the new pokemon
-                print('\nNew Pokemon: ' + str(new_pokemon))
+            # check if the random Pokémon is already in the table
+            current_pokemons = scan_pokemon_table()  # Get the current list of Pokémon
+            
+            # check if the randomly selected Pokémon is already in the current Pokémon data
+            if any(pokemon['name'] == random_pokemon for pokemon in current_pokemons):
+                print('\nThis Pokémon is already in our names list: ' + str(random_pokemon))
+            else:  # if random Pokémon is not in the table
+                new_index_pok = download_details(random_pokemon, data_names_urls)  # Get index for the new Pokémon
+                new_pokemon = insert_pokemon_data(make_file(data_names_urls, new_index_pok))  # Create JSON for the new Pokémon
+                print('\nNew Pokémon added: ' + str(new_pokemon))
 
-                # update the existing list and write to output.json
-                current_pokemons.append(new_pokemon)  # add the new pokemon to the list
-                updated_json = update_json_file(current_pokemons)  # update the JSON file
-                show_data = extract_data_from_json()  # get updated details
-
-        else:  # user_input equal to any input
+        else:  # user input is not 'y'
             # give a farewell greeting to the user and exit
-            print('\nGoodBye!\n')
+            print('\nGoodbye!\n')
             valid_flag = True
 
-
-
 # run the project
-collect_pokemons()
-
+if __name__ == '__main__':
+    create_dynamodb_table()  
+    collect_pokemons()
